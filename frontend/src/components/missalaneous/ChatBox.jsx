2b5cf2602b141873710ea71cd6fa28fbfe574466
Darshan <darshanv487@gmail.com>
2023-12-11 20:21:40 +0530

    Implemented Ui for Group chat and one on one chat

--- frontend/src/components/missalaneous/ChatBox.jsx
+++ frontend/src/components/missalaneous/ChatBox.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { ChatState1 } from "../../Context1/ChatProvider1";
+import { Box } from "@chakra-ui/react";
+import SingleChat from "../SingleChat";
 
-const ChatBox = () => {
-  return <div>this is chat box</div>;
+const ChatBox = ({fetchAgain,setFetchAgain}) => {
+  const { selectedChat } = ChatState1();
+
+  return (
+    <Box
+      display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "68%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>
+    </Box>
+  );
 };
 
 export default ChatBox;
