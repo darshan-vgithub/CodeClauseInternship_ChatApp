32fd81dd48e468715cf173c8dded1c7e71d33acb
Darshan <darshanv487@gmail.com>
2023-12-13 12:36:17 +0530

    Messages Api implemented

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("../backend/routes/userRoutes");
 const chatRoutes=require("../backend/routes/chatRoutes")
+const messageRoutes=require("../backend/routes/messageRoutes")
 const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -13,21 +14,10 @@ const app = express();
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("API is Running successfully");
-// });
-
-// app.get("/api/chat",(req,res)=>{
-//     res.send(chats)
-// })
-
-// app.get("/api/chat/:id",(req,res)=>{
-//     const SingleChat=chats.find((c)=>c._id===req.params.id)
-//     res.send(SingleChat)
-// })
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 
 app.use(notFound);
